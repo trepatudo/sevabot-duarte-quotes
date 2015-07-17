48c3efbca2437ffe701dc67a308660e6560683ec
Marco <dev@marcosilva.pt>
2015-07-17 14:56:37 +0100

    Fix !quote command
    There was a typo at the end of the file

--- modules/quote.sh
+++ modules/quote.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-perl -e 'srand; rand($.) < 1 && ($line = $_) while <>; print $line;' ./duarte-quotes/quotes`
+perl -e 'srand; rand($.) < 1 && ($line = $_) while <>; print $line;' ./duarte-quotes/quotes
