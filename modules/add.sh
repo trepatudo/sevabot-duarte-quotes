c04bc250b76e0bb05720fd95c401d7ad84372685
PatricCunha <patriccunha@outlook.com>
2015-07-21 10:22:04 +0100

    Update add.sh

--- modules/add.sh
+++ modules/add.sh
@@ -6,10 +6,12 @@ then
 	echo "eu quero e que tu te fodas joao tiago"
 	exit 1;
 fi
+# check blacklist
 while read -r line || [[ -n $line ]]; do
 	if [[ $line == *"$SKYPE_USERNAME"* ]]	
 	then
-  		echo "eu quero e que tu te fodas ${line##*-}"
+# not allowed, tell user to fuck off
+  		echo "eu quero e que tu te fodas $SKYPE_FULLNAME"
   		exit 1;
 	fi
 done < ./duarte-quotes/blacklist
