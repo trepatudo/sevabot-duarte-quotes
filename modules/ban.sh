a3f4ec588ea8ce4c7364090b06e6c9b4b3761a59
PatricCunha <patriccunha@outlook.com>
2015-07-21 10:23:58 +0100

    Update ban.sh

--- modules/ban.sh
+++ modules/ban.sh
@@ -2,7 +2,9 @@
 while read -r line || [[ -n $line ]]; do
 	if [[ $line == *"$SKYPE_USERNAME"* ]]	
 	then
+# user is a mod, go ahead and ban
 	    echo "$*" >> ./duarte-quotes/blacklist
+# rub it in
   		echo "tens que ficar interdito $*"
   		exit 1;
 	fi
