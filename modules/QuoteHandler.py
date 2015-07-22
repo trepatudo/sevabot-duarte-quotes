fc5414d17f151c4696a4b26dfe23447124169351
Marco Silva <dev@marcosilva.pt>
2015-07-22 17:38:10 +0100

    QuoteHandler for python big update
    !add, !rem, !list and !meke are now fully working
    Cleaned up some useless imports
    Cleaned up the old bash script modules

--- modules/QuoteHandler.py
+++ modules/QuoteHandler.py
@@ -9,14 +9,15 @@ Handle quotes from Duarte and store them in orchestrator
 
 from __future__ import unicode_literals
 
-
+# Import shitloads of needed stuff
 import logging
-import Skype4Py
 import datetime
 import hashlib
-import random
 import urllib
+import random
+import requests
 
+# Sevabot & Orchestratre
 from sevabot.bot.stateful import StatefulSkypeHandler
 from sevabot.utils import ensure_unicode
 from porc import Client
@@ -34,7 +35,6 @@ logger.debug('[QuoteManager] Loading class')
 
 
 class QuoteHandler(StatefulSkypeHandler):
-
     """
     QuoteHandler
     Handle Duarte's quotes
@@ -52,12 +52,9 @@ class QuoteHandler(StatefulSkypeHandler):
         Set-up our state. This is called every time module is (re)loaded.
         :param skype: Handle to Skype4Py instance
         """
-
         logger.debug('[QuoteManager] Initialized')
 
-        self.skype = sevabot.getSkype()
-
-        self.commands = {'!add': self.quote_add, '!remove': self.quote_remove, '!list': self.quote_list}
+        self.commands = {'!add': self.quote_add, '!rem': self.quote_rem, '!list': self.quote_list, '!meke': self.quote_random }
 
     def handle_message(self, msg, status):
         """
@@ -68,7 +65,7 @@ class QuoteHandler(StatefulSkypeHandler):
         body = ensure_unicode(msg.Body)
 
         # Debug
-        logger.debug('QuoteHandler handler got: {}'.format(body))
+        logger.debug('[QuoteManager] got: {}'.format(body))
 
         # If the separators are not specified, runs of consecutive
         # whitespace are regarded as a single separator
@@ -78,6 +75,7 @@ class QuoteHandler(StatefulSkypeHandler):
         if not len(args):
             return False
 
+        # Find command
         for name, cmd in self.commands.items():
             if name == args[0]:
                 cmd(msg, status, args)
@@ -89,7 +87,7 @@ class QuoteHandler(StatefulSkypeHandler):
         """
         Called when the module is reloaded.
         """
-        logger.debug('QuoteHandler shutdown')
+        logger.debug('[QuoteManager]  shutdown')
 
     def getAll(self):
         """
@@ -99,37 +97,67 @@ class QuoteHandler(StatefulSkypeHandler):
         quotes = pages.all()
 
         return quotes
+
     def quote_add(self, msg, status, args):
         """
-
+        Add a quote, use md5 hashed text as key
         """
+
         # Parse quote and hash it for key
         quote = "".join(args[1:])
-        key = hashlib.md5(quote).hexdigest()
+        m = hashlib.md5()
+        m.update(quote)
+        key = m.hexdigest()
 
         # Save on database with key hashed
         response = client.put("quotes", key, {
-          "text": quote,
-          "author_id": msg.Sender.Handle,
-          "author_name": msg.Sender.FullName,
-          "time_created": str(datetime.datetime.now().isoformat())
-        });
+            "text": quote,
+            "author_id": '- pre-python list -',
+            "author_name": '- pre-python list -',
+            "time_created": str(datetime.datetime.now().isoformat())
+        })
 
         # make sure the request succeeded
-        response.raise_for_status()
+        try:
+            response.raise_for_status()
+            msg.Chat.SendMessage(settings.MESSAGES['add_success'])
+        except:
+            msg.Chat.SendMessage(settings.MESSAGES['add_fail'])
+            pass
 
-
-    def quote_remove(self, msg, status, args):
+    def quote_rem(self, msg, status, args):
+        """
+        Remove a quote based on it's hash
         """
 
-        """
+        # Parse key
+        key = "".join(args[1:])
 
-        # Dirty workaround to start a conference call from a chat
-        call_command = self.skype.Command('CALL {}'.format(chat_name))
-        self.skype.SendCommand(call_command)
+        # @TODO: Maybe check if the quote really exists? For now I don't think its necessary.
+
+        # Save on database with key hashed
+        response = client.delete("quotes", key)
+
+        # make sure the request succeeded
+        msg.Chat.SendMessage("test")
+        print response.status_code
+        try:
+            response.raise_for_status()
+            msg.Chat.SendMessage(settings.MESSAGES['rem_success'])
+        except:
+            msg.Chat.SendMessage(settings.MESSAGES['rem_fail'])
+            pass
+
 
     def quote_random(self, msg, status, args):
+        """
+        Pick a random quote and send it
+        """
         quotes = self.getAll()
+
+        # Send it
+        msg.Chat.SendMessage("%s" % (random.choice(quotes)['value']['text']))
+
     def quote_list(self, msg, status, args):
         """
         Upload a list to sprunge.us
@@ -138,21 +166,23 @@ class QuoteHandler(StatefulSkypeHandler):
         # Get all quotes
         quotes = self.getAll()
 
-        # Get quotes text and break line
+        # Get quotes text and break line (utf8 !)
         quotesString = []
         for s in quotes:
-            quotesString.append(s['path']['key'] + ": "+ s['value']['text'])
+            string = s['path']['key'] + ": " + s['value']['text']
+            quotesString.append(string)
 
         # Prepare upload
         url = 'http://sprunge.us'
-        data = {
-            'sprunge':'\n'.join(quotesString),
-            'submit':'Submit'
-        }
+        # Hardcoded POST string due to UTF-8 crap
+        data = 'sprunge='+ '\n'.join(quotesString) + '&submit=Submit'
+
         # Upload and return url
-        response = urllib.urlopen(url, urllib.urlencode(data))
+        response = urllib.urlopen(url, data.encode('utf-8'))
         page = response.read()
-        print page
+
+        # Send it
+        msg.Chat.SendMessage("%s" % (page))
 
 
 # Export the instance to Sevabot
--- modules/lista.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-awk 'BEGIN{i=1} /.*/{printf "%d: % s\n",i,$0; i++}' ./duarte-quotes/quotes | curl -s -F 'sprunge=<-' http://sprunge.us
